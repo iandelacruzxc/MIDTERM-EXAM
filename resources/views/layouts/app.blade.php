beacf014899cb7ada94704166ad5f12134986457
wendyszxc <162492189+wendyszxc@users.noreply.github.com>
2025-06-01 23:25:22 +0800

    changes of title in web

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <title>University CRUD</title>
+    <title>MANAGEMENT SYSTEM</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
